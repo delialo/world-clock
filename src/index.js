6c7913099001ad7ed7d1a8b19872dac28c315662
D3l14936 <yuu.miyamichi@gmail.com>
2025-07-29 17:15:05 +0200

    Added list of countries, improved UX,

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function updateTime() {
   let denmarkTime = moment().tz("Europe/Copenhagen");
   denmarkDateElement.innerHTML = denmarkTime.format("MMMM Do, yyyy");
   denmarkTimeElement.innerHTML = `${denmarkTime.format(
-    "h:mm:ss[<small>] A [</small>]"
+    "h:mm[<small>] A [</small>]"
   )}`;
   //Detroit
   let usaElement = document.querySelector("#usa");
@@ -14,9 +14,7 @@ function updateTime() {
   let usaTimeElement = usaElement.querySelector(".time");
   let usaTime = moment().tz("America/Detroit");
   usaDateElement.innerHTML = usaTime.format("MMMM Do, yyyy");
-  usaTimeElement.innerHTML = `${usaTime.format(
-    "h:mm:ss[<small>] A [</small>]"
-  )}`;
+  usaTimeElement.innerHTML = `${usaTime.format("h:mm[<small>] A [</small>]")}`;
 
   //Tokyo
   let japanElement = document.querySelector("#japan");
@@ -25,19 +23,22 @@ function updateTime() {
   let japanTime = moment().tz("Asia/Tokyo");
   japanDateElement.innerHTML = japanTime.format("MMMM Do, yyyy");
   japanTimeElement.innerHTML = `${japanTime.format(
-    "h:mm:ss[<small>] A [</small>]"
+    "h:mm[<small>] A [</small>]"
   )}`;
 }
 
 //display selected city time and date
 function showTime(event) {
-  console.log(event);
-  if (event.target.value.length > 0) {
-    let currentDate = moment().tz(event.target.value).format("MMMM Do, yyyy");
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  if (cityTimeZone.length > 0) {
+    let currentDate = moment().tz(cityTimeZone).format("MMMM Do, yyyy");
     let currentTime = moment()
-      .tz(event.target.value)
-      .format("h:mm:ss[<small>] A [</small>]");
-    let currentCity = event.target.value.replace("_", " ").split("/")[1];
+      .tz(cityTimeZone)
+      .format("h:mm[<small>] A [</small>]");
+    let currentCity = cityTimeZone.replace("_", " ").split("/")[1];
     let userCityTime = document.querySelector("#userCityTime");
     let userCityDate = document.querySelector("#userCityDate");
     let userCityName = document.querySelector("#userCityName");
