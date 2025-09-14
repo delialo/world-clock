896feb6caf3dba37d1a3d99c389de6d6e9fe11c0
delialo <yuu.miyamichi@gmail.com>
2025-09-14 14:07:50 +0200

    finally added seconds to update in sync and adjusted the colors again

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+let selectedCityTimeZone = null; // keep track of selectedCity
+
+function updateSelectedCityDisplay() {
+  if (!selectedCityTimeZone) return;
+
+  let currentDate = moment().tz(selectedCityTimeZone).format("MMMM Do, yyyy");
+  let currentTime = moment()
+    .tz(selectedCityTimeZone)
+    .format("h:mm:ss[<small>] A [</small>]");
+  let currentCity = selectedCityTimeZone.replace("_", " ").split("/")[1];
+
+  let userCityTime = document.querySelector("#userCityTime");
+  let userCityDate = document.querySelector("#userCityDate");
+  let userCityName = document.querySelector("#userCityName");
+
+  userCityTime.innerHTML = currentTime;
+  userCityDate.innerHTML = currentDate;
+  userCityName.innerHTML = currentCity;
+}
+
+function showTime(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
+  if (cityTimeZone.length > 0) {
+    selectedCityTimeZone = cityTimeZone;
+    updateSelectedCityDisplay();
+  }
+}
+
 function updateTime() {
   //denmark
   let denmarkElement = document.querySelector("#denmark");
@@ -6,15 +38,18 @@ function updateTime() {
   let denmarkTime = moment().tz("Europe/Copenhagen");
   denmarkDateElement.innerHTML = denmarkTime.format("MMMM Do, yyyy");
   denmarkTimeElement.innerHTML = `${denmarkTime.format(
-    "h:mm[<small>] A [</small>]"
+    "h:mm:ss[<small>] A [</small>]"
   )}`;
+
   //Detroit
   let usaElement = document.querySelector("#usa");
   let usaDateElement = usaElement.querySelector(".date");
   let usaTimeElement = usaElement.querySelector(".time");
   let usaTime = moment().tz("America/Detroit");
   usaDateElement.innerHTML = usaTime.format("MMMM Do, yyyy");
-  usaTimeElement.innerHTML = `${usaTime.format("h:mm[<small>] A [</small>]")}`;
+  usaTimeElement.innerHTML = `${usaTime.format(
+    "h:mm:ss[<small>] A [</small>]"
+  )}`;
 
   //Tokyo
   let japanElement = document.querySelector("#japan");
@@ -23,29 +58,9 @@ function updateTime() {
   let japanTime = moment().tz("Asia/Tokyo");
   japanDateElement.innerHTML = japanTime.format("MMMM Do, yyyy");
   japanTimeElement.innerHTML = `${japanTime.format(
-    "h:mm[<small>] A [</small>]"
+    "h:mm:ss[<small>] A [</small>]"
   )}`;
-}
-
-//display selected city time and date
-function showTime(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
-  if (cityTimeZone.length > 0) {
-    let currentDate = moment().tz(cityTimeZone).format("MMMM Do, yyyy");
-    let currentTime = moment()
-      .tz(cityTimeZone)
-      .format("h:mm[<small>] A [</small>]");
-    let currentCity = cityTimeZone.replace("_", " ").split("/")[1];
-    let userCityTime = document.querySelector("#userCityTime");
-    let userCityDate = document.querySelector("#userCityDate");
-    let userCityName = document.querySelector("#userCityName");
-    userCityTime.innerHTML = currentTime;
-    userCityDate.innerHTML = currentDate;
-    userCityName.innerHTML = currentCity;
-  }
+  updateSelectedCityDisplay();
 }
 
 updateTime();
